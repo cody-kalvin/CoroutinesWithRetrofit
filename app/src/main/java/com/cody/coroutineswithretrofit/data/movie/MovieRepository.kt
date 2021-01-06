a93b40bb86f23eceb59b53710fa3e45d36b6776d
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-06 11:22:26 +0800

    hide API keys

--- app/src/main/java/com/cody/coroutineswithretrofit/data/movie/MovieRepository.kt
+++ app/src/main/java/com/cody/coroutineswithretrofit/data/movie/MovieRepository.kt
@@ -1,8 +1,8 @@
 package com.cody.coroutineswithretrofit.data.movie
 
+import com.cody.coroutineswithretrofit.BuildConfig
 import com.cody.coroutineswithretrofit.api.ApiResult
 import com.cody.coroutineswithretrofit.api.ApiService
-import com.cody.coroutineswithretrofit.helper.Link
 import com.cody.coroutineswithretrofit.helper.apiCall
 import kotlinx.coroutines.CoroutineDispatcher
 import retrofit2.await
@@ -12,6 +12,6 @@ class MovieRepository(
     private val dispatcher: CoroutineDispatcher
 ) {
     suspend fun search(query: String): ApiResult<MovieListResponse> = apiCall(dispatcher) {
-        service.searchMovies(Link.API_KEY, query).await()
+        service.searchMovies(BuildConfig.TMDB_KEY, query).await()
     }
 }
--- app/src/main/java/com/cody/coroutineswithretrofit/helper/Constant.kt
+++ app/src/main/java/com/cody/coroutineswithretrofit/helper/Constant.kt
@@ -3,7 +3,5 @@ package com.cody.coroutineswithretrofit.helper
 object Link {
     const val API_URL = "https://api.themoviedb.org/3/"
 
-    const val API_KEY = "159dfa406cfff892c6ab869e719700cb" // TODO: Do not expose API key here
-
     const val IMG_URL = "https://image.tmdb.org/t/p/original/"
 }
