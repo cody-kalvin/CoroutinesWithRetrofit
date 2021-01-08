4a001a9d8c40658437fe64281ba2be61eb669ffc
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-08 17:22:41 +0800

    support app bar variations

--- app/src/main/java/com/cody/coroutineswithretrofit/ui/movie/search/MovieSearchFragment.kt
+++ app/src/main/java/com/cody/coroutineswithretrofit/ui/movie/search/MovieSearchFragment.kt
@@ -9,6 +9,9 @@ import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cody.coroutineswithretrofit.databinding.FragmentMovieSearchBinding
 
@@ -39,6 +42,12 @@ class MovieSearchFragment : Fragment() {
         binding = FragmentMovieSearchBinding.inflate(layoutInflater, container, false)
         binding.lifecycleOwner = this
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
