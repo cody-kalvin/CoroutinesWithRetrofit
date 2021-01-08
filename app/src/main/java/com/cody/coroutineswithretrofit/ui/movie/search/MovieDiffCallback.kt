d4f90bc846ad3f2e4e906c47dad2ee9703e8dd8d
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-08 11:25:20 +0800

    fix: movie item diff callback error

--- app/src/main/java/com/cody/coroutineswithretrofit/ui/movie/search/MovieDiffCallback.kt
+++ app/src/main/java/com/cody/coroutineswithretrofit/ui/movie/search/MovieDiffCallback.kt
@@ -5,18 +5,36 @@ import com.cody.coroutineswithretrofit.ui.movie.search.MovieListAdapter.MovieLis
 
 class MovieDiffCallback : DiffUtil.ItemCallback<MovieListItem>() {
     override fun areItemsTheSame(oldItem: MovieListItem, newItem: MovieListItem): Boolean {
-        return if (oldItem is MovieListItem.Body && newItem is MovieListItem.Body) {
-            oldItem.movie.id == newItem.movie.id
-        } else {
-            oldItem is MovieListItem.Empty && newItem is MovieListItem.Empty
+        return when {
+            oldItem is MovieListItem.Error && newItem is MovieListItem.Error -> {
+                true
+            }
+            oldItem is MovieListItem.Body && newItem is MovieListItem.Body -> {
+                oldItem.movie.id == newItem.movie.id
+            }
+            oldItem is MovieListItem.Empty && newItem is MovieListItem.Empty -> {
+                oldItem.message == newItem.message
+            }
+            else -> {
+                false
+            }
         }
     }
 
     override fun areContentsTheSame(oldItem: MovieListItem, newItem: MovieListItem): Boolean {
-        return if (oldItem is MovieListItem.Body && newItem is MovieListItem.Body) {
-            oldItem.movie == newItem.movie
-        } else {
-            oldItem is MovieListItem.Empty && newItem is MovieListItem.Empty
+        return when {
+            oldItem is MovieListItem.Error && newItem is MovieListItem.Error -> {
+                true
+            }
+            oldItem is MovieListItem.Body && newItem is MovieListItem.Body -> {
+                oldItem.movie == newItem.movie
+            }
+            oldItem is MovieListItem.Empty && newItem is MovieListItem.Empty -> {
+                oldItem.message == newItem.message
+            }
+            else -> {
+                false
+            }
         }
     }
 }
