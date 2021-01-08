df5c69ed46d917cdb9c3c387f299a70eb870522e
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-08 17:02:22 +0800

    enable data binding for main activity

--- app/src/main/java/com/cody/coroutineswithretrofit/MainActivity.kt
+++ app/src/main/java/com/cody/coroutineswithretrofit/MainActivity.kt
@@ -2,10 +2,12 @@ package com.cody.coroutineswithretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.cody.coroutineswithretrofit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
