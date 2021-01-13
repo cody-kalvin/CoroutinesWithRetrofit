ff98eda37425aa26a124fc0d15f68e657e30cdd2
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-13 14:44:05 +0800

    fix: serialized name for movie genres

--- app/src/main/java/com/cody/coroutineswithretrofit/model/Movie.kt
+++ app/src/main/java/com/cody/coroutineswithretrofit/model/Movie.kt
@@ -15,9 +15,12 @@ data class Movie(
     @SerializedName("poster_path")
     val posterPath: String?,
 
+    @SerializedName("backdrop_path")
+    val backdropPath: String?,
+
     @SerializedName("overview")
     val overview: String?,
 
-    @SerializedName("genres")
+    @SerializedName("genre_ids")
     val genres: List<Genre?>?
 )
